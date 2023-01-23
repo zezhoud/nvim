b3eeb8575e11008a6853e67bd1cd548123611a8b
zezhoud <zezhoud@outlook.com>
2023-01-23 20:15:08 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("plugins.plugins-setup")
+
+require("core.options")
+require("core.keymaps")
+
+-- 插件
+require("plugins.lualine")
---
+++ lua/plugins/plugins-setup.lua
@@ -0,0 +1,39 @@
+-- 自动安装packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- 保存此文件自动更新安装软件
+-- 注意PackerCompile改成了PackerSync
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  
+  -- 主题
+  use 'folke/tokyonight.nvim'
+
+  -- 状态栏
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
