316875a6681d46e0fe843ae98203742a6ecc9783
zezhoud <zezhoud@outlook.com>
2023-01-24 22:09:55 +0800

    windows mangement

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,7 +17,7 @@ keymap.set("n", "s", "<nop>") -- 关闭s键
 keymap.set("n", "S", ":w<CR>") -- 保存
 keymap.set("n", "<leader>w", ":w!<CR>") -- 快速保存
 keymap.set("n", "Q", ":q<CR>") -- 退出
-keymap.set("n", "<C-q>", ":qa<CR>") -- 退出
+keymap.set("n", "<C-q>", ":qa<CR>") -- 全部退出
 
 -- 移动
 keymap.set("n", "J", "5j") -- 光标向下移动5行 
@@ -31,9 +31,30 @@ keymap.set("n", "E", "5e") -- 光标向后移动5个单词
 -- 窗口
 keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
 keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set("n", "<leader>h", "<C-w>h") -- 窗口聚焦向左
+keymap.set("n", "<leader>j", "<C-w>j") -- 窗口聚焦向下
+keymap.set("n", "<leader>k", "<C-w>k") -- 窗口聚焦向上
+keymap.set("n", "<leader>l", "<C-w>l") -- 窗口聚焦向右
+keymap.set("n", "<up>", ":res +5<CR>") -- 窗口向上5行
+keymap.set("n", "<down>", ":res -5<CR>") -- 窗口向下5行
+keymap.set("n", "<left>", ":vertical resize -5<CR>") -- 窗口向左5列
+keymap.set("n", "<right>", ":vertical resize +5<CR>") -- 窗口向右5列
+keymap.set("n", "<LEADER>q", "<C-w>j:q<CR>") -- 关闭当前窗口下方的窗口
+
+-- 页签
+keymap.set("n", "tn", ":tabe<CR>") -- 创建新页签
+keymap.set("n", "th", ":-tabnex<CR>") -- 下一个页签
+keymap.set("n", "tl", ":+tabnext<CR>") -- 上一个页签
+keymap.set("n", "tmh", ":-tabmove<CR>") -- 页签向左移动
+keymap.set("n", "tml", ":+tabmove<CR>") -- 页签向右移动
 
 -- 搜索
+keymap.set("n", "=", "n")
+keymap.set("n", "-", "N")
 keymap.set("n", "<leader><CR>", ":nohl<CR>")
+
+-- 拼写检查
+keymap.set("n", "<leader>ss", ":setlocal spell!<CR>")
 
 -- 切换buffer
 keymap.set("n", "<C-L>", ":bnext<CR>")
